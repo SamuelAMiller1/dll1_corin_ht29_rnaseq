46a37f6612e68fda6befbe1eb4ac3b776ccce8ec
rpolicastro <rpolicas@indiana.edu>
2021-02-08 15:21:51 -0500

    fixing parts of RNA-seq script

--- dll1_corin_rnaseq/rnaseq.sh
+++ dll1_corin_rnaseq/rnaseq.sh
@@ -7,6 +7,7 @@ cp -r /N/project/OHagan_single_cell/ILMN_906_OHagan_BL_mRNAseq24_Dec2020/ /N/sla
 
 mkdir -p sequences
 
+## This move command doesn't do anything.
 mv ILMN_906_OHagan_BL_mRNAseq24_Dec2020/sequences/
 
 gunzip -r ./sequences
@@ -16,8 +17,6 @@ gunzip -r ./sequences
 conda activate rnaseq
 
 #Unload system perl for compatibility with entrez-direct
-
-module unload perl
 
 ANNOTATION='ftp://ftp.ensembl.org/pub/release-99/gtf/homo_sapiens/Homo_sapiens.GRCh38.99.gtf.gz'
 ASSEMBLY='ftp://ftp.ensembl.org/pub/release-91/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz'
@@ -34,11 +33,11 @@ mkdir -p genome
 
 # Download and unpack the genome assembly
 
-curl $ASSEMBLY|gunzip > ./genome/assembly.fasta
+curl $ASSEMBLY | gunzip > ./genome/assembly.fasta
 
 #Download and unpack the genome annotation
 
-curl $ANNOTATION|gunzip > ./genome/annotation.gtf
+curl $ANNOTATION | gunzip > ./genome/annotation.gtf
 
 #Create a directory to store the index
 
@@ -48,14 +47,11 @@ mkdir -p genome/index
 # Argument values are specified to minimize memory usage
 
 STAR \
---runThreadN 8 \
---genomeDir genome/index \
---genomeFastaFiles genome/assembly.fasta \
---sjdbGTFfile genome/annotation.gtf \
---genomeSAindexNbases 12 \
---genomeSAsparseD 3 \
---genomeChrBinNbits=16 \
---limitGenomeGenerateRAM=119000000000
+  --runThreadN 8 \
+  --genomeDir genome/index \
+  --genomeFastaFiles genome/assembly.fasta \
+  --sjdbGTFfile genome/annotation.gtf \
+  --limitGenomeGenerateRAM=119000000000
 
 #############################
 ## Aligned Reads to Genome ##
@@ -69,24 +65,21 @@ mkdir -p results/aligned
 
 FASTQS=($(find ./sequences -name "*\.fastq"))
 
-FASTQSONE=('echo ${FASTQS[@]} | sed 's/ /\n/g' | grep R1_001')
-FASTQSTWO=('echo ${FASTQS[@]} | sed 's/ /\n/g' | grep R2_001')
+FASTQSONE=($(echo ${FASTQS[@]} | grep "R1"))
+FASTQSTWO=($(echo ${FASTQS[@]} | grep "R2"))
 
 for n in ${0..23}; do
-R1=${FASTQSONE[${n}]}
-R2=${FASTQSTWO[${n}]}
+  R1=${FASTQSONE[${n}]}
+  R2=${FASTQSTWO[${n}]}
 
-PREFIX=results/aligned/$(basename ${FASTQS[@]} .fastq)_
+  PREFIX=results/aligned/$(basename ${R1} .fastq)_
 
-STAR \
---runThreadN 8 \
---runMode alignReads \
---genomeLoad \
---LoadAndKeep \
---genomeDir genome/index \
---readFilesIn ${R1} ${R2} \
---outFilesNamePrefix ${PREFIX} \
---outSAMtype BAM SortedByCoordinate
+  STAR \
+    --runThreadN 8 \
+    --genomeDir genome/index \
+    --readFilesIn ${R1} ${R2} \
+    --outFilesNamePrefix ${PREFIX} \
+    --outSAMtype BAM SortedByCoordinate
 done
 
 # Indexing the BAM files
@@ -94,7 +87,7 @@ done
 BAMS=($(find ./results/aligned -name "*\.bam"))
 
 for BAM in ${BAMS[@]}; do
-samtools index $BAM
+  samtools index $BAM
 done
 
 ####################
@@ -108,19 +101,16 @@ done
 ##Additional test code...
 
 # Other test code for star alignment
-for ((n = 0; n < ${#FASTQSONE[23]}; n++)); do 
-R1=${FASTQSONE[${n}]} 
-R2=${FASTQSTWO[${n}]} 
-PREFIX=results/aligned/$(basename ${FASTQSONE[${@}]} .fastq)_ 
-STAR 
---runThreadN 8
---runMode alignReads
---genomeLoad
---LoadAndKeep 
---genomeDir genome/index 
---readFilesIn ${R1} ${R2} 
---outFileNamePrefix ${PREFIX} 
---outSAMtype BAM SortedByCoordinate 
+for ((n = 0; n < ${#FASTQSONE[n]}; n++)); do 
+  R1=${FASTQSONE[${n}]} 
+  R2=${FASTQSTWO[${n}]} 
+  PREFIX=results/aligned/$(basename ${FASTQSONE[${@}]} .fastq)_ 
+  STAR 
+    --runThreadN 8 \
+    --genomeDir genome/index \
+    --readFilesIn ${R1} ${R2} \
+    --outFileNamePrefix ${PREFIX} \
+    --outSAMtype BAM SortedByCoordinate
 done
 
 ## new test code under development for STAR alignment
